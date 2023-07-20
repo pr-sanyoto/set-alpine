efae7b56745f2037af876623b9c8105291e6ad62
pr-sanyoto <91648247+pr-sanyoto@users.noreply.github.com>
2023-07-20 12:50:13 +0700

    Add files via upload

---
+++ set-alpine.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+sed -i 's/#\/media\/cdrom\/apks//g' /etc/apk/repositories
+sed -i 's/#//g' /etc/apk/repositories
+sed -i 's/#Port 22/Port 2203/g' /etc/ssh/sshd_config
+rc-service sshd restart
+apk update
+apk add sudo nano htop iftop curl wget bind-tools net-tools git unzip tzdata open-vm-tools open-vm-to>rc-update add open-vm-tools boot
+rc-service open-vm-tools start
+rc-update add docker boot
+adduser administrator docker
+echo '%wheel ALL=(ALL) ALL' > /etc/sudoers.d/wheel
+adduser administrator wheel
+reboot
