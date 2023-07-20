e26ea14e3361c00d855e631ea760967818ab5b0f
pr-sanyoto <91648247+pr-sanyoto@users.noreply.github.com>
2023-07-20 13:04:39 +0700

    Update set-alpine.sh

--- set-alpine.sh
+++ set-alpine.sh
@@ -4,7 +4,8 @@ sed -i 's/#//g' /etc/apk/repositories
 sed -i 's/#Port 22/Port 2203/g' /etc/ssh/sshd_config
 rc-service sshd restart
 apk update
-apk add sudo nano htop iftop curl wget bind-tools net-tools git unzip tzdata open-vm-tools open-vm-to>rc-update add open-vm-tools boot
+apk add sudo nano htop iftop curl wget bind-tools net-tools git unzip tzdata open-vm-tools open-vm-tools-guestinfo open-vmtools-deploypkg docker docker-bash-completion docker-cli-compose
+rc-update add open-vm-tools boot
 rc-service open-vm-tools start
 rc-update add docker boot
 adduser administrator docker
